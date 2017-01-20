a6d234d475a69482c1f26f132b941dd606ad13b9
jscarsbrook <jscarsbrook@gmail.com>
2017-01-20 18:05:05 +1300

    Commit on 2017-01-20 18:05:04.882000

--- waveBasedSurvivalRTS/app.js
+++ waveBasedSurvivalRTS/app.js
@@ -1,6 +1,25 @@
-window.addEventListener("DOMContentReady", function () {
+var Map = (function () {
+    function Map() {
+    }
+    Map.prototype.draw = function (ctx) {
+    };
+    return Map;
+}());
+window.addEventListener("DOMContentLoaded", function () {
     var mainCanvas = document.querySelector("#mainCanvas");
+    mainCanvas.width = window.innerWidth;
+    mainCanvas.height = window.innerHeight;
+    var ctx = mainCanvas.getContext("2d");
+    var renderableList = [];
+    var map = new Map();
+    renderableList.push(map);
     function update() {
+        ctx.fillStyle = "cornflowerBlue";
+        ctx.fillRect(0, 0, mainCanvas.width, mainCanvas.height);
+        renderableList.forEach(function (renderable) {
+            renderable.draw(ctx);
+        });
+        window.requestAnimationFrame(update);
     }
     window.requestAnimationFrame(update);
 });
--- waveBasedSurvivalRTS/app.ts
+++ waveBasedSurvivalRTS/app.ts
@@ -1,9 +1,34 @@
-window.addEventListener("DOMContentReady", function () {
-    var mainCanvas = document.querySelector("#mainCanvas");
-    
+interface Renderable {
+    draw(ctx: CanvasRenderingContext2D);
+}
+
+class Map implements Renderable {
+    public draw(ctx:CanvasRenderingContext2D) {
+
+    }
+}
+
+window.addEventListener("DOMContentLoaded", function () {
+    var mainCanvas: HTMLCanvasElement = <HTMLCanvasElement> document.querySelector("#mainCanvas");
+    mainCanvas.width =window.innerWidth;
+    mainCanvas.height = window.innerHeight;
+    var ctx: CanvasRenderingContext2D = mainCanvas.getContext("2d");
+
+    var renderableList: Renderable[] = [];
+
+    var map: Map = new Map();
+
+    renderableList.push(map);
 
     function update() {
+        ctx.fillStyle = "cornflowerBlue";
+        ctx.fillRect(0, 0, mainCanvas.width, mainCanvas.height);
 
+        renderableList.forEach(function (renderable: Renderable) {
+            renderable.draw(ctx);
+        })
+
+        window.requestAnimationFrame(update);
     }
 
     window.requestAnimationFrame(update);
