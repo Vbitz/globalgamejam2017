70c40b28479ed8fa280e8c513e2ac7efc908c444
jscarsbrook <jscarsbrook@gmail.com>
2017-01-20 16:09:56 +1300

    Commit on 2017-01-20 16:09:56.391000

--- autocommit.py
+++ autocommit.py
@@ -1,7 +1,9 @@
-import os, sys, datetime
+import os, sys, datetime, time
 from subprocess import check_call
 
 if __name__ == "__main__":
-    dateString = str(datetime.datetime.now())
-    check_call(["git", "add", "."])
-    check_call(["git", "commit", "-m", "Commit on %s" % (dateString, )])
+    while True:
+        dateString = str(datetime.datetime.now())
+        check_call(["git", "add", "."])
+        check_call(["git", "commit", "-m", "Commit on %s" % (dateString, )])
+        time.sleep(60)
