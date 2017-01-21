8b24992b9f854e5f96c9a0a1bc2cfca795704b20
jscarsbrook <jscarsbrook@gmail.com>
2017-01-21 13:27:07 +1300

    Commit on 2017-01-21 13:27:07.801000

--- deadlineSimulator/app.js
+++ deadlineSimulator/app.js
@@ -4,11 +4,8 @@
     Every x seconds the player fights off an advancing force. They have to accure resources by preforming actions to fight the force off.
     This is basicly a resource and time management game.
     Game Over state occours if the player does'nt have enough resources to fight off the wave.
-    One interesting aspect is resources are consumed in the attack. 
+    One interesting aspect is resources are consumed in the attack.
 */
-
 function main() {
-
 }
-
 document.addEventListener("DOMContentLoaded", main);
