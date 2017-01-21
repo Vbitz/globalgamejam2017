df61e71bffc42a68e5481fcb9ecb418fb8b4063b
jscarsbrook <jscarsbrook@gmail.com>
2017-01-21 13:22:03 +1300

    Commit on 2017-01-21 13:22:03.540000

---
+++ deadlineSimulator/app.ts
@@ -0,0 +1,12 @@
+/*
+    Deadline Simulator
+
+    Every x seconds the player fights off an advancing force. They have to accure resources by preforming actions to fight the force off.
+    This is basicly a resource and time management game  
+*/
+
+function main() {
+
+}
+
+document.addEventListener("DOMContentLoaded", main);
