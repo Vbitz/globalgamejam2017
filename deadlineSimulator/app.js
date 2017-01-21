aad6cbff519d8fb7733371c59b326054907f9e4b
jscarsbrook <jscarsbrook@gmail.com>
2017-01-21 15:07:20 +1300

    Commit on 2017-01-21 15:07:20.709000

--- deadlineSimulator/app.js
+++ deadlineSimulator/app.js
@@ -5,7 +5,13 @@
     This is basicly a resource and time management game.
     Game Over state occours if the player does'nt have enough resources to fight off the wave.
     One interesting aspect is resources are consumed in the attack.
+    
+    Savegame Storage is stored in
 */
+function deleteSaveFile() {
+    localStorage.clear();
+    return false;
+}
 function main() {
 }
 document.addEventListener("DOMContentLoaded", main);
